c4adc61561068bbc803d5e118dcb141c0cb561dd
badikgit <49131247+badikgit@users.noreply.github.com>
2023-02-26 18:44:46 +0300

    fix: reset template content

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from './components';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <BrowserRouter>
+    <React.StrictMode>
         <App />
-    </BrowserRouter>
+    </React.StrictMode>
 );
--- src/routes/NotFound/NotFound.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function NotFound() {
-    return <h1>Oops... Error 404. Page not found.</h1>;
-}
